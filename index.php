9a41e176d5c84f178dc2960096153e28f244298c
Iner-17 <inercollado52@gmail.com>
2020-10-05 20:22:23 +0800

    Added a front page

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice</title>
+	<link rel="stylesheet" href="./src/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+
+</head>
+<body>
+
+	<nav>
+		<div class="logo">logo</div>
+		<ul class="header-links">
+			<li><a href="#">home</a></li>
+			<li><a href="#">about</a></li>
+			<li><a href="#">service</a></li>
+			<li><a href="#">places</a></li>
+			<li><a href="#">contact</a></li>
+		</ul>
+		<div class="ctn-form">
+			<a href="#">sign up</a>
+			<a href="#" class="active">log in</a>
+		</div>
+	</nav>
+  	<div class="container">
+		<div class="bg-image"></div>
+		<div class="front-page">
+			<h1>It's More Fun in The Philippines</h1>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus incidunt perspiciatis at dolor facilis. 
+				Hic pariatur dolores sed deserunt quis, magni illo sit. Voluptatem, odio enim. Deserunt atque sunt illum?
+			</p>
+			<a href="#">Why Philippines?<i class="fas fa-fighter-jet"></i></a>
+		</div>
+		<div class="login-form">
+			<form action=""></form>
+		</div>
+	</div>
+    
+
+</body>
+</html>
