dee87ae6b310972f58590e78d73419bda998f054
Iner-17 <inercollado52@gmail.com>
2020-10-07 10:41:07 +0800

    changed the desing of a button

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Practice</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP Practice</title>
 	<link rel="stylesheet" href="./src/style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 
 </head>
+
 <body>
 
 	<nav>
@@ -24,17 +26,18 @@
 			<a href="#" class="active">log in</a>
 		</div>
 	</nav>
-  	<div class="container">
+	<div class="container">
 		<div class="bg-image"></div>
 		<div class="front-page">
 			<h1>It's More Fun in The <span class="ph">Philippines</span></h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus incidunt perspiciatis at dolor facilis. 
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus incidunt perspiciatis at dolor facilis.
 				Hic pariatur dolores sed deserunt quis, magni illo sit. Voluptatem, odio enim. Deserunt atque sunt illum?
 			</p>
 			<a href="#">Why Philippines?<i class="fas fa-fighter-jet"></i></a>
 		</div>
 	</div>
-    
-<script src="src/app.js"></script>
+
+	<script src="src/app.js"></script>
 </body>
+
 </html>
