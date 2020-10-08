e55529db6911fdd40bfa6b70e52c590c28ddbffd
Iner-17 <inercollado52@gmail.com>
2020-10-08 21:09:10 +0800

    deleted the services

--- index.php
+++ index.php
@@ -17,7 +17,6 @@
 		<ul class="header-links">
 			<li><a href="#">home</a></li>
 			<li><a href="#">about</a></li>
-			<li><a href="#">service</a></li>
 			<li><a href="#">places</a></li>
 			<li><a href="#">contact</a></li>
 		</ul>
